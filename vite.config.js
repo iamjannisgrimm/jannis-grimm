8f9a7253035661f2417fe6e8cbf95b1d281c1e98
Jannis <jannis@grimm.me>
2025-04-29 08:05:53 -0400

    super basic working react

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/jannis-grimm/',
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  base: command === 'serve' ? '/' : '/jannis-grimm/', // <<< THIS LINE
+}));
