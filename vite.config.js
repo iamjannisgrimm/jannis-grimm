3f68af566a644a4fe5dc63d12312f5a629dc9890
Jannis <jannis@grimm.me>
2025-04-29 08:17:36 -0400

    Modified config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/jannis-grimm/', // <<< THIS LINE
-}));
+  base: '/jannis-grimm/', // <-- REMOVE the dynamic base nonsense
+});
