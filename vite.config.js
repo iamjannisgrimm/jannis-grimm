8856968446f8ce197575f95ce0b54b8af545a9d7
Jannis <jannis@grimm.me>
2025-04-30 06:58:56 -0400

    modified base to work with GH hosted site

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jannis-grimm/', // <-- REMOVE the dynamic base nonsense
+  base: '/', // <-- REMOVE the dynamic base nonsense
 });
