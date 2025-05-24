18f8b63898e7581a329742a9b0e53a128b77674a
Jannis <jannis@grimm.me>
2025-05-24 13:58:24 -0400

    Updates

--- src/data/quotes.js
+++
@@ -1,6 +0,0 @@
-export const quotes = [
-    {
-      quote: "Driven by innovation and fueled by purpose, I craft exceptional solutions that elevate the human experience. \n\n(And your business proposition)",
-      author: "Jannis Grimm",
-    }
-  ];
