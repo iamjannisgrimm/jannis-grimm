c888e7aa4e224f4999e00b9da005e5b681d2dad3
Jannis Grimm <jannis@grimm.me>
2026-04-07 10:44:55 -0400

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  assetsInclude: ['src/assets/fonts/*.woff2', 'src/assets/fonts/*.woff']
-});
